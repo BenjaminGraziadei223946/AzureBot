e07c6bc5e52d3af4967a2ce7cf22cf2915f529c6
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-02 17:01:54 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -9,25 +9,21 @@ def acceptCall(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event: %s', azeventgrid.event_type)
 
     # Define the webhook URL, which now points to the acsWebhook function within the same Function App
-    webhook_url = 'https://EventCallback.azurewebsites.net/api/acsWebhook'
+    webhook_url = 'https://eventcallback.azurewebsites.net/api/acswebhook'
 
-    if azeventgrid.event_type == 'Microsoft.Communication.IncomingCall':
-        logging.info('Incoming call')
-        bot_data = {
-            'type': 'incomingCall',
-            'data': azeventgrid.get_json()
-        }
-        # Send data to Microsoft Teams bot via a webhook
-        response = requests.post(webhook_url, json=bot_data)
+    logging.info('Incoming call')
+    response = requests.post(webhook_url, json=azeventgrid.get_json())
     return func.HttpResponse("Data forwarded to Microsoft Teams bot", status_code=200)
 
 @app.route(route="acsWebhook", auth_level=func.AuthLevel.ANONYMOUS)
 def acs_webhook(req: func.HttpRequest) -> func.HttpResponse:
     # Extract information from the Event Grid event
-    event_data = req.get_json()  # Assume the request body contains the event data in JSON format
-
-    # Check if the event is an incoming call
-    logging.info('Incoming call')
+    try:
+        request_data = req.get_json()
+    except ValueError as e:
+        logging.error('Failed to parse JSON: %s', e)
+        return func.HttpResponse("Invalid JSON", status_code=400)
     
-
-    return func.HttpResponse("Data forwarded to Microsoft Teams bot", status_code=200)
+    request_data = req.get_json()
+    # ...
+    return func.HttpResponse(body=request_data, status_code=200, mimetype="application/json")
