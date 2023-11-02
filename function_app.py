f5b0e252cf17c513ed3aa0d682ebededf4f8a413
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-02 11:43:31 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,6 +1,6 @@
-import logging
 import azure.functions as func
-
+import logging
+import requests
 
 app = func.FunctionApp()
 
@@ -8,9 +8,29 @@ app = func.FunctionApp()
 def acceptCall(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event: %s', azeventgrid.event_type)
 
+    # Define the webhook URL, which now points to the acsWebhook function within the same Function App
+    webhook_url = 'https://EventCallback.azurewebsites.net/api/acsWebhook'
+
     if azeventgrid.event_type == 'Microsoft.Communication.IncomingCall':
         logging.info('Incoming call')
-    elif azeventgrid.event_type == 'Microsoft.Communication.CallStarted':
-        logging.info('Call started')
-    elif azeventgrid.event_type == 'Microsoft.Communication.CallEnded':
-        logging.info('Call ended')
+        bot_data = {
+            'type': 'incomingCall',
+            'data': azeventgrid.data
+        }
+        # Send data to Microsoft Teams bot via a webhook
+        response = requests.post(webhook_url, json=bot_data)
+
+        logging.info('Sent event data to webhook: %s', response.status_code)
+
+    # ... (handle other event types as needed)
+
+@app.http_trigger(name="acsWebhook", methods=['post'], auth_level='function')
+def acs_webhook(req: func.HttpRequest) -> func.HttpResponse:
+    # Extract information from the Event Grid event
+    event_data = req.get_json()  # Assume the request body contains the event data in JSON format
+
+    # Check if the event is an incoming call
+    logging.info('Incoming call')
+    
+
+    return func.HttpResponse("Data forwarded to Microsoft Teams bot", status_code=200)
